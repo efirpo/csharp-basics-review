ae4293af1631d720f8289aa3649c154c4ec68d42
Ethan Firpo <suzannedeakins@Ethan-Firpo-CyberDeck.local>
2020-05-08 08:45:22 -0700

    add initial classes for bread and pastry

---
+++ Programs/bakery.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+
+namespace Bakery.Order
+{
+  public class Bread
+  {
+    public int _breadOrder { get; set; }
+
+  }
+
+  public class Pastry
+  {
+    public int _pastryOrder { get; set; }
+  }
+}
