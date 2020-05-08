994e87e31a8f1050c6e4ee73f2d16869cb0caacc
Ethan Firpo <suzannedeakins@Ethan-Firpo-CyberDeck.local>
2020-05-08 12:47:24 -0700

    Edit ASCII art

--- Programs/Program.cs
+++ Programs/Program.cs
@@ -11,27 +11,35 @@ namespace PierresBakery
     {
 
       Console.WriteLine("\n                         Welcome to\n\n");
-      Console.WriteLine(" ___  _                     _      ___       _                  ");
-      Console.WriteLine("| . \\<_> ___  _ _  _ _  ___|/___  | . > ___ | |__ ___  _ _  _ _ ");
-      Console.WriteLine("|  _/| |/ ._>| '_>| '_>/ ._><_-<  | . \\<_> || / // ._>| '_>| | |");
-      Console.WriteLine("|_|  |_|\\___.|_|  |_|  \\___./__/  |___/<___||_\\_\\\\___.|_|  `_. |");
-      Console.WriteLine("                                                           <___'\n");
+      Console.WriteLine("'||''|.   ||                                  '                             ");
+      Console.WriteLine(" ||   || ...    ....  ... ..  ... ..    ....     ....                       ");
+      Console.WriteLine(" ||...|'  ||  .|...||  ||' ''  ||' '' .|...||   ||. '                       ");
+      Console.WriteLine(" ||       ||  ||       ||      ||     ||        . '|..                      ");
+      Console.WriteLine(".||.     .||.  '|...' .||.    .||.     '|...'   |'..|'                      \n\n");
+      Console.WriteLine("                        '||''|.           '||                               ");
+      Console.WriteLine("                         ||   ||   ....    ||  ..    ....  ... ..  .... ... ");
+      Console.WriteLine("                         ||'''|.  '' .||   || .'   .|...||  ||' ''  '|.  |  ");
+      Console.WriteLine("                         ||    || .|' ||   ||'|.   ||       ||       '|.|   ");
+      Console.WriteLine("                        .||...|'  '|..'|' .||. ||.  '|...' .||.       '|    ");
+      Console.WriteLine("                                                                   .. |     ");
+      Console.WriteLine("                                                                    ''     ");
+
       Console.WriteLine("\nBread costs $5.00 per loaf, and when you buy 2 you get 1 free!\n\nPastries cost $2.00, or 3 for $5.00!\n\nWould you like to place an order? [Y/N]");
       string confirm = (Console.ReadLine().ToLower());
       if (confirm == "y")
       {
-        Console.WriteLine("Would you \n\t1) Like to get extra loaves on top of your order\n\tor \n\t2) Order how many you want total and let us calculate how many should be free? [ 1 / 2 ]");
+        Console.WriteLine("Would you \n\t1) Like to get extra bread loaves on top of your ordered amount\n\n\tor \n\n\t2) Order how many you want total and let us calculate how many should be free? [ 1 / 2 ]");
         string confirmBread = (Console.ReadLine().ToLower());
-        Console.WriteLine("How many loaves of bread would you like?");
+        Console.WriteLine("\nHow many loaves of bread would you like?");
         int breadOrder = int.Parse(Console.ReadLine());
-        Console.WriteLine("How many pastries would you like?");
+        Console.WriteLine("\nHow many pastries would you like?");
         int pastryOrder = int.Parse(Console.ReadLine());
         Bread bread = new Bread(breadOrder, confirmBread);
         Pastry pastry = new Pastry(pastryOrder);
-        Console.WriteLine("Thank you for your order! Your order details are below.");
-        Console.WriteLine(bread.CalculateBread());
-        Console.WriteLine(pastry.CalculatePastry());
-        Console.WriteLine("Your total order will cost: $" + ((bread._breadPrice) + (pastry._pastryPrice)) + ".00!");
+        Console.WriteLine("\nThank you for your order! Your order details are below.");
+        Console.WriteLine("\n" + bread.CalculateBread());
+        Console.WriteLine("\n" + pastry.CalculatePastry());
+        Console.WriteLine("\nYour total order will cost: $" + ((bread._breadPrice) + (pastry._pastryPrice)) + ".00!");
 
 
 
