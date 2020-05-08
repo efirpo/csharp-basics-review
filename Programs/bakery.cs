475a92275684d786f71a120e38c37d29e380bce2
Ethan Firpo <suzannedeakins@Ethan-Firpo-CyberDeck.local>
2020-05-08 09:21:37 -0700

    add basic calculations for pastry prices

--- Programs/bakery.cs
+++ Programs/bakery.cs
@@ -7,10 +7,37 @@ namespace Bakery.Order
   {
     public int _breadOrder { get; set; }
 
+    public Bread(int amount)
+    {
+      _breadOrder = amount;
+    }
   }
 
   public class Pastry
   {
     public int _pastryOrder { get; set; }
+    public int _pastryPrice { get; set; }
+    public Pastry(int amount)
+    {
+      _pastryOrder = amount;
+
+    }
+
+    public string CalculatePastry()
+    {
+      if (_pastryOrder > 3)
+      {
+        _pastryPrice = (((_pastryOrder - (_pastryOrder % 3) / 3) * 5) + ((_pastryOrder % 3) * 2));
+      }
+      else if (_pastryOrder == 3)
+      {
+        _pastryPrice = 5;
+      }
+      else if (_pastryOrder < 3)
+      {
+        _pastryPrice = _pastryOrder * 2;
+      }
+    }
   }
+
 }
